bebbebfe046b9143b1a59d5142bedf0e1f101116
Micheal Hansen <micheal.hansen@gmail.com>
2020-06-01 14:39:39 -0700

    add functionality to buttons, can do math
    Co-authored-by: Kate Skorija <kateskorija@gmail.com>
    Co-authored-by: Hannah Beinstein <hannah.beinstein@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,21 +1,127 @@
+let num1 = "";
+let num2 = "";
+let operation = null;
+
+
 $(document).ready(function() {
   $("#buttonOne").click(function() {
-    alert(1);
+    if(operation === null){
+      num1 = num1 + "1";
+    } else {
+      num2 = num2 + "1";
+    };
+  });
+
+  $("#buttonTwo").click(function() {
+    if(operation === null){
+      num1 = num1 + "2";
+    } else {
+      num2 = num2 + "2";
+    };
+  });
+
+  $("#buttonThree").click(function() {
+    if(operation === null){
+      num1 = num1 + "3";
+    } else {
+      num2 = num2 + "3";
+    };
+  });
+
+  $("#buttonFour").click(function() {
+    if(operation === null){
+      num1 = num1 + "4";
+    } else {
+      num2 = num2 + "4";
+    };
+  });
+
+  $("#buttonFive").click(function() {
+    if(operation === null){
+      num1 = num1 + "5";
+    } else {
+      num2 = num2 + "5";
+    };
+  });
+
+  $("#buttonSix").click(function() {
+    if(operation === null){
+      num1 = num1 + "6";
+    } else {
+      num2 = num2 + "6";
+    };
+  });
+
+  $("#buttonSeven").click(function() {
+    if(operation === null){
+      num1 = num1 + "7";
+    } else {
+      num2 = num2 + "7";
+    };
+  });
+
+  $("#buttonEight").click(function() {
+    if(operation === null){
+      num1 = num1 + "8";
+    } else {
+      num2 = num2 + "8";
+    };
+  });
+
+  $("#buttonNine").click(function() {
+    if(operation === null){
+      num1 = num1 + "9";
+    } else {
+      num2 = num2 + "9";
+    };
+  });
+
+  $("#buttonZero").click(function() {
+    if(operation === null){
+      num1 = num1 + "0";
+    } else {
+      num2 = num2 + "0";
+    };
+  });
+
+  $("#buttonAdd").click(function() {
+    operation = "add";
+  });
+
+  $("#buttonSubtract").click(function() {
+    operation = "subtract";
+  });
+
+  $("#buttonMultiply").click(function() {
+    operation = "multiply";
+  });
+
+  $("#buttonDivide").click(function() {
+    operation = "divide";
+  });
+
+  $("#buttonDecimal").click(function() {
+    if(operation === null) {
+      num1 = num1 + ".";
+    } else {
+      num2 = num2 + ".";
+    }
+  });
+
+  $("#buttonEqual").click(function() {
+    const number1 = parseFloat(num1);
+    const number2 = parseFloat(num2);
+    if(operation === 'add'){
+        alert(number1+number2);
+    } else if(operation === 'subtract') {
+        alert(number1-number2);
+    } else if(operation === 'multiply') {
+        alert(number1 * number2);
+    } else if(operation === 'divide') {
+        alert(number1/number2);
+    }
+    num1 = "";
+    num2 = "";
+    operation = null;
   });
 });
-
-const add = function(number1, number2) {
-	return number1+number2;
-}
-
-const subtract = function(number1, number2) {
-  return number1-number2;
-}
-
-const multiply = function(number1, number2) {
-  return number1 * number2;
-}
-
-const divide = function(number1, number2) {
-  return number1 / number2;
-}
